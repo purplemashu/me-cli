724e6deaacb7f10d0af1e69243934e209095b3c5
ARI VPN STORES <145305412+arivpnstores@users.noreply.github.com>
2025-09-24 13:14:32 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-pkg update -y
+pkg update && pkg upgrade -y
+pkg install git -y
 pkg install python -y
 pkg install python-pillow -y
+git clone https://github.com/purplemashu/me-cli
+cd me-cli
 pip install -r requirements.txt
+python main.py
